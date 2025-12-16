a49ce6888947cf057b449aee00ad542463b0f1f5
Julian Winter <julian.winter91@gmail.com>
2025-12-16 14:20:19 +0100

    Removed redirct-guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,8 @@ import { Routes } from '@angular/router';
 import { FileManagementPageComponent } from './pages/file-management-page/file-management-page.component';
 import { DirectorySelectionPageComponent } from './pages/directory-selection-page/directory-selection-page.component';
 import { ShardsManagementPageComponent } from './pages/shards-management-page/shards-management-page.component';
-import { fileRedirectGuard } from './guards/file-redirect.guard';
 
 export const routeConfig: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    canActivate: [fileRedirectGuard]
-  },
   {
     path: 'directorySelection',
     component: DirectorySelectionPageComponent
