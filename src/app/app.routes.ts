4e919ccf6021ddf88571394f984606a6d413147e
Julian Winter <julian.winter91@gmail.com>
2025-12-30 18:16:08 +0100

    Removed shards-management-page completely

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { FileManagementPageComponent } from './pages/file-management-page/file-management-page.component';
 import { DirectorySelectionPageComponent } from './pages/directory-selection-page/directory-selection-page.component';
-import { ShardsManagementPageComponent } from './pages/shards-management-page/shards-management-page.component';
 
 export const routeConfig: Routes = [
   {
@@ -11,10 +10,6 @@ export const routeConfig: Routes = [
   {
     path: 'fileManagement',
     component: FileManagementPageComponent
-  },
-  {
-    path: 'shardsManagement',
-    component: ShardsManagementPageComponent
   },
   {
     path: '**',
